5567e76aef32f3ca92bc7cbf71b03509376bf81a
waledapo <waledapo12@gmail.com>
2022-10-14 03:55:34 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main- The entry point for the program
+ *Return: by default returns zero
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
