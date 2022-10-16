23c4ff3ef3e99dc63fc7901c43e3670a62a14094
waledapo <waledapo12@gmail.com>
2022-10-16 15:49:17 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main-entry point
+ * Return:zero for correct
+ */
+int main(void)
+{
+	int i;
+	for (i = '0'; i <= '9'; i++)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
