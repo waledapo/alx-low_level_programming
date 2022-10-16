6a58d17370948f4f73629cf4c2e4e7ff4bea8e23
waledapo <waledapo12@gmail.com>
2022-10-16 13:37:14 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main-entry poin
+ * Return:zero if correct
+ */
+int main(void)
+{
+	char i;
+
+
+	for (i = 'a'; i <= 'z'; i++)
+{
+	putchar(i);
+}
+	putchar('\n');
+	return (0);
+}
