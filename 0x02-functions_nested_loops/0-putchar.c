047c8f2aa96b722721c972c4b26dfc7bd64a441e
waledapo <waledapo12@gmail.com>
2022-10-18 17:16:46 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main-entry point
  * Return:zero when correct
@@ -10,8 +10,8 @@ int main(void)
 
 	for (i = 0; i <= 8; i++)
 	{
-	putchar(word[i]);	}
+	_putchar(word[i]);	}
 
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }
