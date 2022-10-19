246c08f2dc1cde2d6cbb042f2cc8c796ad6e9286
waledapo <waledapo12@gmail.com>
2022-10-19 15:15:57 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs-it gives absolute value
+ * @n: n is the integer
+ * Return: positive for correct and negative otherwise
+ */
+	int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
