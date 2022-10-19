c3a280f80462d26f0a9c2cbfdfec51b3922a7cad
waledapo <waledapo12@gmail.com>
2022-10-18 17:41:17 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,10 +5,13 @@
  */
 int main(void)
 {
-void print_alphabet(void);
-char i;
-for (i = 'a'; i <= 'z'; i++)
+	void print_alphabet(void);
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
 	_putchar(i);
+
+
 	_putchar('\n');
 
 	return (0);
