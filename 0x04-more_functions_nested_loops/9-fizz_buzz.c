b6818b3d1add2f14620c3b833963aa0ea139a7d5
waledapo <waledapo12@gmail.com>
2022-10-23 12:04:58 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main-prints 1-10 and multiple of 3 as fizz and buzz
+ * Return:zero
+ */
+	int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 3) == 0)
+			printf("FIZZ");
+		else if ((num % 5) == 0)
+			printf("BUZZ");
+		else if ((num % 3) == 0 && (num % 5) == 0)
+			printf("FIZZBUZZ");
+		else
+			printf("%d", num);
+
+		if (num == 100)
+			continue;
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
