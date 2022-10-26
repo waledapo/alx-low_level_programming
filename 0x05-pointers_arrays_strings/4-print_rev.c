34d1edb9fe74a836e9707da7e59cc4b0904a0726
waledapo <waledapo12@gmail.com>
2022-10-25 17:54:58 -0700

    My reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev- a function that prints a string, in reverse, followed by a new
+ * line
+ * @s: the s passed
+ * Return: nothing
+ */
+	void print_rev(char *s)
+{
+	int string = 0;
+
+	while (s[string] != '\0')
+		string++;
+	while (string)
+		_putchar(s[--string]);
+	_putchar('\n');
+}
