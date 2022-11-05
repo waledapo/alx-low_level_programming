223c2e68131b1347c498661f12b59d685cd11845
waledapo <waledapo12@gmail.com>
2022-11-05 16:29:38 -0700

    my name is for args

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,9 @@
  * @argv:character vector
  * Return:always zero
  */
-	int main(int __attribute__((unused)) argc, char *argv[])
+	int main(int argc, char *argv[])
 {
-	printf("%s/n", argv[0]);
+	if (argc > 0)
+		printf("%s/n", argv[0]);
 	return (0);
 }
