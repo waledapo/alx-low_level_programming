12a0687a0dbef395c3ca92384b9f7403ecaee9d0
waledapo <waledapo12@gmail.com>
2022-11-05 16:07:28 -0700

    My name args

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: character count
+ * @argv:character vector
+ * Return:always zero
+ */
+	int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s/n", argv[0]);
+	return (0);
+}
